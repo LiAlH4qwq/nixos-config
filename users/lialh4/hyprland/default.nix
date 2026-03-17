8031ad0d911a4f6a811ccf2ff1796b6e4caa586a
LiAlH4 <lialh4qwq@outlook.com>
2026-03-17 11:25:17 +0800

    use lighter script impl

--- users/lialh4/hyprland/default.nix
+++ users/lialh4/hyprland/default.nix
@@ -6,8 +6,8 @@ _: {
     bind = [
       "$mainMod, Delete, exec, uwsm stop"
       "$mainMod, Q, killactive"
-      "$mainMod, T exec, uwsm app -- $terminal"
-      "$mainMod, B, exec, uwsm app -- $browser"
+      "$mainMod, T exec, uwsm-app -- $terminal"
+      "$mainMod, B, exec, uwsm-app -- $browser"
     ];
   };
 }
