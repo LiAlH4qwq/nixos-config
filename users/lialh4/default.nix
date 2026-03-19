cdf4d4e977c95a15ff22e45b7a0826a58f07f795
LiAlH4 <lialh4qwq@outlook.com>
2026-03-20 00:06:50 +0800

    add fcitx5 addons

--- users/lialh4/default.nix
+++ users/lialh4/default.nix
@@ -31,6 +31,11 @@
         waylandFrontend = true;
         addons = with pkgs; [
           fcitx5-gtk
+          fcitx5-rose-pine
+          fcitx5-pinyin-zhwiki
+          fcitx5-pinyin-moegirl
+          fcitx5-pinyin-minecraft
+          kdePackages.fcitx5-qt
           kdePackages.fcitx5-chinese-addons
         ];
       };
