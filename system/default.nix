e4ce0138131d477c4500c48729df883a69ffbf65
LiAlH4 <lialh4qwq@outlook.com>
2026-03-20 17:30:56 +0800

    allow users to shutdown computer

--- system/default.nix
+++ system/default.nix
@@ -12,7 +12,25 @@
   # replace sudo with run0.
   security = {
     sudo.enable = false;
-    polkit.enable = true;
+    polkit = {
+      enable = true;
+      extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if (
+            subject.isInGroup("users")
+              && (
+                action.id == "org.freedesktop.login1.reboot" ||
+                action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+                action.id == "org.freedesktop.login1.power-off" ||
+                action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+              )
+            )
+          {
+            return polkit.Result.YES;
+          }
+        });
+      '';
+    };
   };
 
   networking = {
