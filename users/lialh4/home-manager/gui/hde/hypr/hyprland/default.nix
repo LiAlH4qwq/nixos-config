fc589c2754cdf60d74e94a43a0065d2ac3007e4e
LiAlH4 <lialh4qwq@outlook.com>
2026-03-23 02:04:19 +0800

    use nix vars, not hyprlangs, to avoid conflicting with env vars, and for better performance due to vars are inlined in generated config file

--- users/lialh4/home-manager/gui/hde/hypr/hyprland/default.nix
+++ users/lialh4/home-manager/gui/hde/hypr/hyprland/default.nix
@@ -4,81 +4,90 @@
     enable = true;
     # Due to conflict with home-manager
     systemd.enable = false;
-    settings = {
-      "$statusbar" = "ashell";
-      "$launcher" = "vicinae toggle";
-      "$clipboard" = "vicinae vicinae://extensions/vicinae/clipboard/history";
-      "$terminal" = "kitty";
-      "$explorer" = "nautilus -w";
-      "$browser" = "firefox";
-      "$pwd" = "1password";
-      "$mainMod" = "SUPER";
-      exec-once = [
-        "uwsm-app -t service -s s -u $statusbar.service -- $statusbar"
-        "[workspace name:Password silent] uwsm-app -t service -s b -u $pwd.service -- $pwd"
-      ];
-      general = {
-        border_size = 4;
-        # Look like double? Actually they are same!
-        gaps_in = 0;
-        gaps_out = 0;
-        "col.active_border" = "0xffd7827e";
-        "col.inactive_border" = "0xff9893a5";
-      };
-      monitor = [
-        ", preferred, auto, 2"
-      ];
-      input.natural_scroll = true;
-      input.touchpad.natural_scroll = true;
-      bind = [
-        "$mainMod, Delete, execr, loginctl kill-session $XDG_SESSION_ID"
-        "$mainMod, Q, killactive"
-        "$mainMod SHIFT, Q, forcekillactive"
-        "$mainMod, R, execr, uwsm-app -- $launcher"
-        "$mainMod, V, execr, uwsm-app -- $clipboard"
-        "$mainMod, T, execr, uwsm-app -- $terminal"
-        "$mainMod, E, execr, uwsm-app -- $explorer"
-        "$mainMod, B, execr, uwsm-app -- $browser"
-        "$mainMod ALT, Tab, workspace, empty"
-        "$mainMod ALT SHIFT, Tab, movetoworkspace, empty"
-        # Screenshot keys on Thinkbook 14 G4+ IAP.
-        # Screenshot key of this device is hardcoded to Win + Shift + S.
-        # Screenshot key.
-        "$mainMod SHIFT, S, execr, uwsm-app -- hyprshot -zm region"
-        # Fn + Screenshot key.
-        ", Print, execr, uwsm-app -- hyprshot -zm window"
-        # Fn + Ctrl + Screenshot key.
-        "CTRL, Print, execr, uwsm-app -- hyprshot -zm output"
-        ", XF86Favorites, workspace, name:Password"
-        "SHIFT, XF86Favorites, movetoworkspace, name:Password"
-      ]
-
-      ++ (lib.concatMap (
-        k:
-        let
-          ks = toString k;
-          w = if k == 0 then 10 else k;
-          ws = toString w;
-        in
-        [
-          "$mainMod, ${ks}, workspace, ${ws}"
-          "$mainMod SHIFT, ${ks}, movetoworkspace, ${ws}"
+    settings =
+      let
+        mod = "SUPER";
+        startService = target: "uwsm-app -s s -t service -u uwsm-service-${target}.service -- ${target}";
+        startBg = target: "uwsm-app -s b -t service -u uwsm-bg-${target}.service -- ${target}";
+        statusbar = "ashell";
+        launcher = "vicinae toggle";
+        clipboard = "vicinae vicinae://extensions/vicinae/clipboard/history";
+        terminal = "kitty";
+        explorer = "nautilus -w";
+        taskmgr = "mission-center";
+        browser = "firefox";
+        pwd = "1password";
+      in
+      {
+        general = {
+          border_size = 4;
+          gaps_in = 0;
+          gaps_out = 0;
+          "col.active_border" = "0xffd7827e";
+          "col.inactive_border" = "0xff9893a5";
+        };
+        input = {
+          natural_scroll = true;
+          touchpad.natural_scroll = true;
+        };
+        monitor = [
+          ", preferred, auto, 2"
+        ];
+        exec-once = [
+          (startService statusbar)
+          (startBg pwd)
+        ];
+        bind = [
+          "${mod}, Delete, execr, loginctl kill-session $XDG_SESSION_ID"
+          "${mod}, Q, killactive"
+          "${mod} SHIFT, Q, forcekillactive"
+          "${mod}, R, execr, uwsm-app -- ${launcher}"
+          "${mod}, V, execr, uwsm-app -- ${clipboard}"
+          "${mod}, T, execr, uwsm-app -- ${terminal}"
+          "${mod}, E, execr, uwsm-app -- ${explorer}"
+          "${mod}, Escape, execr, uwsm-app -- ${taskmgr}"
+          "${mod}, B, execr, uwsm-app -- ${browser}"
+          "${mod} ALT, Tab, workspace, empty"
+          "${mod} ALT SHIFT, Tab, movetoworkspace, empty"
+          # Screenshot keys on Thinkbook 14 G4+ IAP.
+          # Screenshot key of this device is hardcoded to Win + Shift + S.
+          # Fn + Screenshot key is Screen/Sysrq
+          # Screenshot key.
+          "SUPER SHIFT, S, execr, uwsm-app -- hyprshot -zm region"
+          # Fn + Screenshot key.
+          ", Print, execr, uwsm-app -- hyprshot -zm window"
+          # Fn + Ctrl + Screenshot key.
+          "CTRL, Print, execr, uwsm-app -- hyprshot -zm output"
+          "${mod}, XF86Favorites, workspace, name:Password"
+          "${mod} SHIFT, XF86Favorites, movetoworkspace, name:Password"
         ]
-      ) (lib.range 0 9));
-      bindl = [
-        ", XF86AudioMute, execr, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        ", XF86AudioMicMute, execr, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-      ];
-      binde = [
-        "$mainMod, Tab, workspace, +1"
-        "$mainMod SHIFT, Tab, workspace, -1"
-      ];
-      bindle = [
-        ", XF86AudioRaiseVolume, execr, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-        ", XF86AudioLowerVolume, execr, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        ", XF86MonBrightnessUp, execr, brightnessctl set 5%+"
-        ", XF86MonBrightnessDown, execr, brightnessctl set 5%-"
-      ];
-    };
+
+        ++ (lib.concatMap (
+          k:
+          let
+            ks = toString k;
+            w = if k == 0 then 10 else k;
+            ws = toString w;
+          in
+          [
+            "${mod}, ${ks}, workspace, ${ws}"
+            "${mod} SHIFT, ${ks}, movetoworkspace, ${ws}"
+          ]
+        ) (lib.range 0 9));
+        bindl = [
+          ", XF86AudioMute, execr, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ", XF86AudioMicMute, execr, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ];
+        binde = [
+          "${mod}, Tab, workspace, +1"
+          "${mod} SHIFT, Tab, workspace, -1"
+        ];
+        bindle = [
+          ", XF86AudioRaiseVolume, execr, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+          ", XF86AudioLowerVolume, execr, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          ", XF86MonBrightnessUp, execr, brightnessctl set 5%+"
+          ", XF86MonBrightnessDown, execr, brightnessctl set 5%-"
+        ];
+      };
   };
 }
