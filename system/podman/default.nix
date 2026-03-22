f8030c9e60d9b0a84af9621403010cfb9d1448df
LiAlH4 <lialh4qwq@outlook.com>
2026-03-22 23:27:00 +0800

    split modules from non-modules

---
+++ system/podman/default.nix
@@ -0,0 +1,27 @@
+{ config, lib, ... }:
+{
+  options.liuxu.system.podman.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    example = true;
+    description = ''
+      Liuxu: Whether to enable Podman,
+        a container management tool,
+        drop-in replacement of docker.
+    '';
+  };
+
+  config = lib.mkIf config.liuxu.system.podman.enable {
+    virtualisation = {
+      containers.enable = true;
+      oci-containers.backend = "podman";
+      podman = {
+        enable = true;
+        dockerCompat = true;
+        dockerSocket.enable = true;
+        # allow communication between containers.
+        defaultNetwork.settings.dns_enabled = true;
+      };
+    };
+  };
+}
