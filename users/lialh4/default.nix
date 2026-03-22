05925292ef3b5271ddcc7099661e380d59434f62
LiAlH4 <lialh4qwq@outlook.com>
2026-03-23 00:33:12 +0800

    split user and user.home-manager

--- users/lialh4/default.nix
+++ users/lialh4/default.nix
@@ -1,18 +1,4 @@
-{ pkgs, ... }:
-{
-  imports = [
-    ./state
-    ./i18n
-    ./xcursor
-    ./hyprland
-    ./hyprtoolkit
-    ./hyprshot
-    ./mako
-    ./syncthing
-    ./vicinae
-    ./vscode
-  ];
-
+_: {
   users.extraUsers.lialh4 = {
     isNormalUser = true;
     useDefaultShell = true;
@@ -22,54 +8,5 @@
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDT8vagjK0tG4HNgk5vAPtIXyHJdEwg7dNPRsapZIObvShj7I92HxvWPqI/4ApkcWrTAB3z076S4H8cIuGxD4MTV4L55M/o5lfqcqdSlBv5H+u/r74FdQCo8uQXNqxo2gkO1Fhj3Ub8iGXe7nxjn6OUJdBnwDDBU/0YCgVwtiAesvsZwClLt5B0o/1F6MPUGTDQU3QYGV5F8baiabzi1lwh0utViM4PZzU9RbizcTf1nvuYQE9BTJdvgB/7R4/CgRxyURIDBPxznkse6eUMUHrsl/MxRUxsdjJS7b9FVYNpMJ8w53YCa0CBtIu6mXUd63PidmjV+O00RapxK7Q/rgqiaHUAHku8kew6yLBExpQh+d2IlLpijN7hYP7y06/FZcR5/dLXGw3dNKkCLr6lMx0AEAdpethUx5Tby8gkQ5wFO0omH3WS8dsYJ/3d7lOPvtKjEAOXwP23rAmZGgUhl6zK85B2KdROrRPCuzxAOrPevRgtpVhza9EABTzgVrg+jRs="
     ];
   };
-  home-manager.users.lialh4 = {
-    gtk = rec {
-      enable = true;
-      colorScheme = "light";
-      theme = gtk4.theme;
-      iconTheme = gtk4.iconTheme;
-      # gtk4 needs to force enable.
-      gtk4 = {
-        enable = true;
-        theme = {
-          name = "rose-pine-dawn";
-          package = pkgs.rose-pine-gtk-theme;
-        };
-        iconTheme = {
-          name = "rose-pine-dawn";
-          package = pkgs.rose-pine-icon-theme;
-        };
-      };
-    };
-    # these hasn't been available as a program in release 25.11.
-    home.packages = with pkgs; [
-      wev # Input inspect tool
-      nixfmt # Nix file formatter
-      nautilus # Explorer
-      lollypop # Music player
-      clapper # Video player
-      materialgram # Telegram with material design
-      wl-clipboard-rs # Clipboard history of vicinae
-    ];
-    services = {
-      hyprpolkitagent.enable = true;
-      hyprpaper.enable = true;
-    };
-    services.hyprpaper.settings = import ./hyprpaper;
-    # when using genAttrs,
-    # it will broken with error:
-    # programs field already defined.
-    programs = {
-      home-manager.enable = true;
-      fish.enable = true;
-      firefox.enable = true;
-      kitty.enable = true;
-      foot.enable = true;
-      git.enable = true;
-      ashell.enable = true;
-    };
-    programs.ashell.settings = import ./ashell;
-    programs.kitty.settings = import ./kitty;
-    programs.git.settings = import ./git;
-  };
+  home-manager.users.lialh4 = ./home-manager;
 }
---
+++ users/lialh4/home-manager/default.nix
@@ -0,0 +1,66 @@
+{ pkgs, ... }:
+{
+  imports = [
+    ./state
+    ./i18n
+    ./xcursor
+    ./hyprland
+    ./hyprtoolkit
+    ./hyprshot
+    ./mako
+    ./syncthing
+    ./vicinae
+    ./vscode
+  ];
+
+  gtk = rec {
+    enable = true;
+    colorScheme = "light";
+    theme = gtk4.theme;
+    iconTheme = gtk4.iconTheme;
+    # gtk4 needs to force enable.
+    gtk4 = {
+      enable = true;
+      theme = {
+        name = "rose-pine-dawn";
+        package = pkgs.rose-pine-gtk-theme;
+      };
+      iconTheme = {
+        name = "rose-pine-dawn";
+        package = pkgs.rose-pine-icon-theme;
+      };
+    };
+  };
+
+  # these hasn't been available as a program in release 25.11.
+  home.packages = with pkgs; [
+    wev # Input inspect tool
+    nixfmt # Nix file formatter
+    nautilus # Explorer
+    lollypop # Music player
+    clapper # Video player
+    materialgram # Telegram with material design
+    wl-clipboard-rs # Clipboard history of vicinae
+  ];
+
+  services = {
+    hyprpolkitagent.enable = true;
+    hyprpaper.enable = true;
+  };
+
+  # when using genAttrs,
+  # it will broken with error:
+  # programs field already defined.
+  programs = {
+    home-manager.enable = true;
+    fish.enable = true;
+    firefox.enable = true;
+    kitty.enable = true;
+    foot.enable = true;
+    git.enable = true;
+    ashell.enable = true;
+  };
+  programs.ashell.settings = import ./ashell;
+  programs.kitty.settings = import ./kitty;
+  programs.git.settings = import ./git;
+}
