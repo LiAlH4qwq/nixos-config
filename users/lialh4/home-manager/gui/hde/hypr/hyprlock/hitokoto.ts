2c8ed4339086383d27cda1b1075b1c61669586a9
LiAlH4 <lialh4qwq@outlook.com>
2026-03-24 11:05:23 +0800

    add timeout to hitokoto

--- users/lialh4/home-manager/gui/hde/hypr/hyprlock/hitokoto.ts
+++ users/lialh4/home-manager/gui/hde/hypr/hyprlock/hitokoto.ts
@@ -1,8 +1,14 @@
 const main = async () => {
-    const hitokoto = await fetch("https://v1.hitokoto.cn/?c=i")
+    const fallback = {
+        hitokoto: "数人世相逢，百年欢笑，能得几回又。",
+        from: "摸鱼儿·记年时人人何处",
+        from_who: "何梦桂"
+    }
+    const timeout = Bun.sleep(3 * 1000).then(_ => fallback)
+    const online = await fetch("https://v1.hitokoto.cn/?c=i")
         .then(res => res.json())
         .then(res => {
-            if (!(res instanceof Object)) throw new Error()
+            if (typeof res != "object") throw new Error()
             return res
         })
         .then(res => {
@@ -10,20 +16,25 @@ const main = async () => {
             return res
         })
         .then(res => {
+            if (typeof res.hitokoto != "string") throw new Error()
+            return res
+        })
+        .then(res => {
             if (!res.hitokoto) throw new Error()
             return res
         })
-        .catch(_ => ({
-            hitokoto: "数人世相逢，百年欢笑，能得几回又。",
-            from: "摸鱼儿·记年时人人何处",
-            from_who: "何梦桂"
-        }))
+        .catch(_ => fallback)
+    const racing = await Promise.race([timeout, online])
     const formatted = `
-${hitokoto.hitokoto}
-\t——${hitokoto.from_who ? hitokoto.from_who : ""}\
-${hitokoto.from ? `《${hitokoto.from}》` : ""}
+${racing.hitokoto}
+${racing.from || racing.from_who
+            ? `${"\u3000".repeat(racing.hitokoto.length - 2)}——${racing.from_who ? racing.from_who : ""}\
+${racing.from ? `《${racing.from}》` : ""}`
+            : ""
+        }
 `.trim()
     console.log(formatted)
+    process.exit()
 }
 
 main()
