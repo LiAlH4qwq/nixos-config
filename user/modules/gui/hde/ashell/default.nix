ea28105884c3ae85ca58809d73161bec33f78633
LiAlH4 <lialh4qwq@outlook.com>
2026-04-08 16:12:47 +0800

    add power profiles switcher

--- user/modules/gui/hde/ashell/default.nix
+++ user/modules/gui/hde/ashell/default.nix
@@ -1,12 +1,5 @@
 # Rose Pine Dawn Theme
 # Source: https://rosepinetheme.com/palette/
-# Relation: Hyprtoolkit color <--> Rose Pine Dawn color
-# text <--> Text
-# background.base <--> Base
-# background.weak <--> Surface
-# background.strong <--> Overlay
-# primary <--> Rose
-# secondary <--> Love
 
 { config, lib, ... }:
 {
@@ -15,18 +8,43 @@
       enable = true;
       settings = {
         enable_esc_key = true;
-        appearance = {
-          scale_factor = 1.2;
-          style = "Solid";
-          text_color = "#575279";
-          primary_color = "#d7827e";
-          secondary_color = "#b4637a";
-          background_color = {
-            base = "#faf4ed";
-            strong = "#f2e9e1";
-            weak = "#fffaf3";
+        appearance =
+          let
+            cl_text = "#575279";
+            cl_base = "#faf4ed";
+            cl_surface = "#fffaf3";
+            cl_overlay = "#f2e9e1";
+            cl_rose = "#d7827e";
+            cl_love = "#b4637a";
+          in
+          {
+            scale_factor = 1.2;
+            style = "Solid";
+            text_color = {
+              base = cl_text;
+              strong = cl_text;
+              weak = cl_text;
+              text = cl_text;
+            };
+            background_color = {
+              base = cl_base;
+              strong = cl_overlay;
+              weak = cl_surface;
+              text = cl_text;
+            };
+            primary_color = {
+              base = cl_rose;
+              strong = cl_rose;
+              weak = cl_rose;
+              text = cl_text;
+            };
+            secondary_color = {
+              base = cl_love;
+              strong = cl_love;
+              weak = cl_love;
+              text = cl_text;
+            };
           };
-        };
         modules = {
           left = [
             "SystemInfo"
