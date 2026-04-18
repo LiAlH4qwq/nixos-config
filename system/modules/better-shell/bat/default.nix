c2f0c42eb39a33224b8d1a0e283232d38cb4c95d
LiAlH4 <lialh4qwq@outlook.com>
2026-04-18 21:46:02 +0800

    adjust starship

--- system/modules/better-shell/bat/default.nix
+++ system/modules/better-shell/bat/default.nix
@@ -6,7 +6,7 @@
         enable = true;
       };
       fish.shellAliases.cat = "bat";
-      bash.shellAliases.cat = "bat";     
+      bash.shellAliases.cat = "bat";
     };
   };
 }
--- system/modules/better-shell/starship/default.nix
+++ system/modules/better-shell/starship/default.nix
@@ -1,5 +1,19 @@
 _: {
   programs.starship = {
     enable = true;
+    settings = {
+      shell = {
+        disabled = false;
+        fish_indicator = "󰈺";
+        bash_indicator = "";
+      };
+      shlvl = {
+        disabled = false;
+        format = "[$symbol]($style)";
+        repeat = true;
+        symbol = "❯";
+        repeat_offset = 1;
+      };
+    };
   };
 }
