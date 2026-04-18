308df7479ad4b638e3509963e9f9d5ecc2e03232
LiAlH4 <lialh4qwq@outlook.com>
2026-04-18 18:58:58 +0800

    fix fish greeting

--- system/modules/better-shell/fish/default.nix
+++ system/modules/better-shell/fish/default.nix
@@ -9,9 +9,9 @@
   config = lib.mkIf config.liuxu.system.better-shell.enable {
     programs.fish = {
       enable = true;
-      shellAliases = {
-        fish_greeting = "";
-      };
+      shellInit = ''
+        set fish_greeting
+      '';
     };
     users.defaultUserShell = pkgs.fish;
   };
