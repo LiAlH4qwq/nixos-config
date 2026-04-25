a7de951726c594e80da6f3a8863baffbe34940cb
LiAlH4 <lialh4qwq@outlook.com>
2026-04-25 23:44:59 +0800

    adjust noctalia

--- user/modules/gui/hde/noctalia/default.nix
+++ user/modules/gui/hde/noctalia/default.nix
@@ -6,6 +6,51 @@
     enable = true;
     settings = {
       settingsVersion = 0;
+      bar = {
+        showCapsule = false;
+        outerCorners = false;
+        widgets = {
+          left = [
+            {
+              id = "SystemMonitor";
+            }
+            {
+              id = "Workspace";
+            }
+            {
+              id = "ActiveWindow";
+            }
+          ];
+          center = [
+            {
+              id = "Clock";
+            }
+          ];
+          right = [
+            {
+              id = "MediaMini";
+            }
+            {
+              id = "Tray";
+            }
+            {
+              id = "NotificationHistory";
+            }
+            {
+              id = "Volume";
+            }
+            {
+              id = "Brightness";
+            }
+            {
+              id = "Battery";
+            }
+            {
+              id = "ControlCenter";
+            }
+          ];
+        };
+      };
       colorSchemes = {
         darkMode = false;
       };
