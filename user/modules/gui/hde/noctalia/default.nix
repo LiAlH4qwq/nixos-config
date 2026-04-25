98aaa886462bba97e6f73d783611aa8eef347c70
LiAlH4 <lialh4qwq@outlook.com>
2026-04-25 23:46:59 +0800

    only enables noctalia when enables gui

--- user/modules/gui/hde/noctalia/default.nix
+++ user/modules/gui/hde/noctalia/default.nix
@@ -1,58 +1,65 @@
-{ inputs, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
 {
   imports = [ inputs.noctalia.homeModules.default ];
 
-  programs.noctalia-shell = {
-    enable = true;
-    settings = {
-      settingsVersion = 0;
-      bar = {
-        showCapsule = false;
-        outerCorners = false;
-        widgets = {
-          left = [
-            {
-              id = "SystemMonitor";
-            }
-            {
-              id = "Workspace";
-            }
-            {
-              id = "ActiveWindow";
-            }
-          ];
-          center = [
-            {
-              id = "Clock";
-            }
-          ];
-          right = [
-            {
-              id = "MediaMini";
-            }
-            {
-              id = "Tray";
-            }
-            {
-              id = "NotificationHistory";
-            }
-            {
-              id = "Volume";
-            }
-            {
-              id = "Brightness";
-            }
-            {
-              id = "Battery";
-            }
-            {
-              id = "ControlCenter";
-            }
-          ];
+  config = lib.mkIf config.liuxu.user.gui.enable {
+    programs.noctalia-shell = {
+      enable = true;
+      settings = {
+        settingsVersion = 0;
+        bar = {
+          showCapsule = false;
+          outerCorners = false;
+          widgets = {
+            left = [
+              {
+                id = "SystemMonitor";
+              }
+              {
+                id = "Workspace";
+              }
+              {
+                id = "ActiveWindow";
+              }
+            ];
+            center = [
+              {
+                id = "Clock";
+              }
+            ];
+            right = [
+              {
+                id = "MediaMini";
+              }
+              {
+                id = "Tray";
+              }
+              {
+                id = "NotificationHistory";
+              }
+              {
+                id = "Volume";
+              }
+              {
+                id = "Brightness";
+              }
+              {
+                id = "Battery";
+              }
+              {
+                id = "ControlCenter";
+              }
+            ];
+          };
         };
-      };
-      colorSchemes = {
-        darkMode = false;
+        colorSchemes = {
+          darkMode = false;
+        };
       };
     };
   };
