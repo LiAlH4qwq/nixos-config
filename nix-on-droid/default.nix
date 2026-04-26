04aaee4f50251a5c9f259540d8d4d3cd9f3a9582
LiAlH4 <lialh4qwq@outlook.com>
2026-04-26 23:18:19 +0800

    droid: fix system packages

--- nix-on-droid/default.nix
+++ nix-on-droid/default.nix
@@ -1,7 +1,29 @@
-{ inputs, ... }:
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [
     "${inputs.self}/system"
     ./proot
   ];
+
+  options.environment.systemPackages = lib.mkOption {
+    type = lib.types.listOf lib.types.package;
+    default = [ ];
+    example = with pkgs; [ git ];
+  };
+
+  config = {
+    environment.packages = config.environment.systemPackages;
+    nixpkgs.overlays = [
+      inputs.self.overlays.default
+      inputs.rust-overlay.overlays.default
+      inputs.ragenix.overlays.default
+      inputs.deploy-rs.overlays.default
+    ];
+  };
 }
--- parts/overlays/default.nix
+++ parts/overlays/default.nix
@@ -1,19 +1,8 @@
 { inputs, ... }:
 {
-  perSystem =
-    { pkgs, ... }:
-    let
-      pkgsWithBun2Nix = pkgs.extend inputs.bun2nix.overlays.default;
-    in
-    {
-      packages.hitokoto = pkgsWithBun2Nix.callPackage "${inputs.self}/packages/hitokoto" { };
-      packages.hyprlock-hint = pkgsWithBun2Nix.callPackage "${inputs.self}/packages/hyprlock-hint" { };
-    };
   flake = {
     overlays = {
       default = final: prev: {
-        hitokoto = inputs.self.packages.${prev.stdenv.hostPlatform.system}.hitokoto;
-        hyprlock-hint = inputs.self.packages.${prev.stdenv.hostPlatform.system}.hyprlock-hint;
         zellij = prev.zellij.override (old: {
           rustPlatform = final.makeRustPlatform {
             cargo = final.rust-bin.stable.latest.minimal;
