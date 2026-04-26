aad5b197cbfc30733c90dfc745a0374b7b1028b0
LiAlH4 <lialh4qwq@outlook.com>
2026-04-26 17:11:29 +0800

    remove Paimon

--- devices/htpc/users/root/default.nix
+++ devices/htpc/users/root/default.nix
@@ -1,7 +1,7 @@
 _: {
   users.extraUsers.root = {
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOrK8/XY2U6HQJ79HourV5SBele6qPn83iAwBda6xozd"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKPzvkOPfWZmx2zE6cJY4Qz+Z1dKXTgd6Y2I/RgIc86T"
     ];
   };
 }
