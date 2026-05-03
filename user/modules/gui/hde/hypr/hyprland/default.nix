5fc3d50583a4294c1bf8d30672c5c812d62f1964
LiAlH4 <lialh4qwq@outlook.com>
2026-05-04 01:38:36 +0800

    make clementine setting window float to avoid buh

--- user/modules/gui/hde/hypr/hyprland/default.nix
+++ user/modules/gui/hde/hypr/hyprland/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./noctalia
+    ./wrule
   ];
 
   config = lib.mkIf config.liuxu.user.gui.enable {
@@ -77,25 +78,6 @@
             "${mod}, mouse:272, movewindow"
             "${mod} ALT, mouse:272, resizewindow 1" # Keep aspect ratio
             "${mod} ALT SHIFT, mouse:272, resizewindow 2" # Ignore aspect ratio
-          ];
-          # No border, rounding, shadow when only one window.
-          workspace = [
-            "w[tv1], gapsout:0, gapsin:0"
-            "f[1], gapsout:0, gapsin:0"
-          ];
-          windowrule = [
-            "noborder, floating:0, onworkspace:w[tv1]"
-            "noborder, floating:0, onworkspace:f[1]"
-            "norounding, floating:0, onworkspace:w[tv1]"
-            "norounding, floating:0, onworkspace:f[1]"
-            "noshadow, floating:0, onworkspace:w[tv1]"
-            "noshadow, floating:0, onworkspace:f[1]"
-            "suppressevent maximize, class:.*"
-            # I don't know why,
-            # but calced position like 100%-w(weight) 100%-h(height)
-            # just doesn't work,
-            # maybe it doesn't support lazy evaluation like nix :(
-            "float, pin, size 25% 25%, move 75% 75%, initialClass:^firefox$, initialTitle:^Picture-in-Picture$"
           ];
         };
     };
---
+++ user/modules/gui/hde/hypr/hyprland/wrule/default.nix
@@ -0,0 +1,27 @@
+{ config, lib, ... }:
+{
+  config = lib.mkIf config.liuxu.user.gui.enable {
+    wayland.windowManager.hyprland.settings = {
+      # No border, rounding, shadow when only one window.
+      workspace = [
+        "w[tv1], gapsout:0, gapsin:0"
+        "f[1], gapsout:0, gapsin:0"
+      ];
+      windowrule = [
+        "noborder, floating:0, onworkspace:w[tv1]"
+        "noborder, floating:0, onworkspace:f[1]"
+        "norounding, floating:0, onworkspace:w[tv1]"
+        "norounding, floating:0, onworkspace:f[1]"
+        "noshadow, floating:0, onworkspace:w[tv1]"
+        "noshadow, floating:0, onworkspace:f[1]"
+        "suppressevent maximize, class:.*"
+        # I don't know why,
+        # but calced position like 100%-w(weight) 100%-h(height)
+        # just doesn't work,
+        # maybe it doesn't support lazy evaluation like nix :(
+        "float, pin, size 25% 25%, move 75% 75%, initialClass:^firefox$, initialTitle:^Picture-in-Picture$"
+        "float, initialClass:^org.clementine_player.Clementine$, initialTitle:^Preferences$"
+      ];
+    };
+  };
+}
