e6d497bd2b65648b800165b6486210e162f56887
LiAlH4 <lialh4qwq@outlook.com>
2026-05-09 15:38:37 +0800

    i18n: fix ime for wechat and wps

--- parts/overlays/default.nix
+++ parts/overlays/default.nix
@@ -6,6 +6,23 @@
         unstable = import inputs.nixpkgs-unstable {
           inherit (prev) config system;
         };
+        wechat = prev.symlinkJoin {
+          name = "wechat";
+          paths = [ prev.wechat ];
+          nativeBuildInputs = [ prev.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/wechat \
+              --set QT_IM_MODULE fcitx \
+          '';
+        };
+        wpsoffice-cn = prev.wpsoffice-cn.overrideAttrs (old: {
+          nativeBuildInputs = old.nativeBuildInputs ++ [ prev.makeWrapper ];
+          postFixup = (old.postFixup or "") + ''
+            for bin in $out/bin/*; do                                                                                                                    
+              wrapProgram "$bin" --set QT_IM_MODULE fcitx                                                                                                
+            done
+          '';
+        });
         zellij = prev.zellij.override (old: {
           rustPlatform = final.makeRustPlatform {
             cargo = final.rust-bin.stable.latest.minimal;
