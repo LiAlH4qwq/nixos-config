95bcda5d711d0509ddf8174ec396ab639ea9d9dd
LiAlH4 <lialh4qwq@outlook.com>
2026-06-19 01:36:56 +0800

    fix noctalia flake

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@ in
       };
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia/legacy-v4";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         noctalia-qs.follows = "noctalia-qs";
