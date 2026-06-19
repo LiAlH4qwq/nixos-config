cb7b7f4f17dcec3011de37307dfdbe8c4c9dcfe4
LiAlH4 <lialh4qwq@outlook.com>
2026-06-19 18:14:37 +0800

    make autostart async

--- home/modules/gui/hde/hypr/hyprland/autostart/default.nix
+++ home/modules/gui/hde/hypr/hyprland/autostart/default.nix
@@ -7,18 +7,20 @@
   };
 
   config = lib.mkIf config.liuxu.home.gui.enable {
-    # Fix fcitx5 won't work.
-    liuxu.home.internal.gui.autostart = lib.singleton "fcitx5 -rd";
-    wayland.windowManager.hyprland.settings.on._args = [
-      "hyprland.start"
-      (lib.generators.mkLuaInline ''
-        function()
-          ${
-            map (e: ''hl.exec_cmd("${e}")'') config.liuxu.home.internal.gui.autostart
-            |> lib.concatStringsSep "\n  "
-          } 
-        end
-      '')
+    liuxu.home.internal.gui.autostart = [
+      # Fix fcitx5 won't work.
+      "fcitx5 -rd"
+      "1password --silent"
     ];
+    wayland.windowManager.hyprland.settings.on = map (e: {
+      _args = [
+        "hyprland.start"
+        (lib.generators.mkLuaInline ''
+          function()
+            hl.exec_cmd("${e}")
+          end
+        '')
+      ];
+    }) config.liuxu.home.internal.gui.autostart;
   };
 }
