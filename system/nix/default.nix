408e839855c07fa419b96d339d0dc650c497277e
LiAlH4 <lialh4qwq@outlook.com>
2026-08-01 09:17:14 +0800

    fix substituters untrusted

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -2,9 +2,14 @@
   imports = [
     ./lix
   ];
-  nix.settings = {
-    substituters = self.nixConfig.extra-substituters;
-    trusted-public-keys = self.nixConfig.extra-trusted-public-keys;
-    experimental-features = self.nixConfig.extra-experimental-features;
-  };
+  nix.settings =
+    let
+      substituters = self.nixConfig.extra-substituters;
+    in
+    {
+      inherit substituters;
+      trusted-substituters = substituters;
+      trusted-public-keys = self.nixConfig.extra-trusted-public-keys;
+      experimental-features = self.nixConfig.extra-experimental-features;
+    };
 }
