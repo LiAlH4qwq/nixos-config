331a8d2168eaab4e40287e044453fb5d799aa662
LiAlH4 <lialh4qwq@outlook.com>
2026-08-03 23:36:24 +0800

    update doc

--- parts/docs/default.nix
+++ parts/docs/default.nix
@@ -8,30 +8,81 @@
   perSystem = { pkgs, system, ... }: {
     packages =
       let
-        eval = lib.nixosSystem {
+        osModules = lib.nixosSystem {
           inherit system;
           specialArgs = {
             inherit inputs self;
             inherit (self) lib;
           };
           modules = [
-            self.nixosModules.liuxu
+            { _module.check = false; }
+            self.nixosModules.default
           ];
         };
-        optionsDoc = pkgs.nixosOptionsDoc {
-          inherit (eval) options;
-          transformOptions =
-            opt:
-            opt
-            // {
-              visible = (builtins.head opt.loc) == "liuxu";
-            };
+        homeModules = inputs.home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          check = false;
+          extraSpecialArgs = {
+            inherit inputs self;
+            inherit (self) lib;
+            osConfig = osModules.config;
+          };
+          modules = [
+            self.homeModules.default
+            {
+              home = {
+                username = "user";
+                homeDirectory = "/home/user";
+              };
+            }
+          ];
         };
-        doc = optionsDoc.optionsCommonMark;
+        toDoc =
+          modules:
+          pkgs.nixosOptionsDoc {
+            inherit (modules) options;
+            warningsAreErrors = false;
+            transformOptions = o: if (builtins.head o.loc) == "liuxu" then o else o // { visible = false; };
+          };
+        toMdDoc = doc: doc.optionsCommonMark;
+        osMdDoc = osModules |> toDoc |> toMdDoc;
+        homeMdDoc = homeModules |> toDoc |> toMdDoc;
+        docPackage = pkgs.stdenv.mkDerivation {
+          pname = "liuxu-options-doc";
+          version = "0";
+
+          nativeBuildInputs = [ pkgs.mdbook ];
+
+          dontUnpack = true;
+
+          buildPhase = ''
+            mkdir ./src
+
+            cat > ./src/SUMMARY.md <<-EOF
+            # Summary
+
+            - [NixOS Options](os-options.md)
+            - [Home Options](home-options.md)
+            EOF
+
+            mdbook init . \
+              --title="liuxu options" \
+              --ignore=none
+
+            cp ${osMdDoc} ./src/os-options.md
+            cp ${homeMdDoc} ./src/home-options.md
+
+            mkdir -p $out
+
+            mdbook build -d $out
+          '';
+
+          dontInstall = true;
+        };
       in
       {
-        inherit doc;
-        default = doc;
+        default = docPackage;
+        doc = docPackage;
       };
   };
 }
