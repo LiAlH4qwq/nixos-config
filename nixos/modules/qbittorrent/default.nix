1c3000bdc55ca6a3ab42503889b1426f36f66d17
LiAlH4 <lialh4qwq@outlook.com>
2026-08-05 14:14:27 +0800

    update qbittorrent module

--- nixos/modules/qbittorrent/default.nix
+++ nixos/modules/qbittorrent/default.nix
@@ -1,13 +1,41 @@
-{ config, lib, ... }: {
+{
+  config,
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [ inputs.peer-ban-helper.nixosModules.default ];
+
   options.liuxu.nixos.qbittorrent.enable = lib.liuxu.modules.mkOsSwitchOnOption ''
-    Whether to enable the qbittorrent service.
+    Whether to enable the qBittorrent service,
+      uses qBittorrent Enhanced Edition,
+      and will also enables Peer Ban Helper.
   '';
 
   config = lib.mkIf config.liuxu.nixos.qbittorrent.enable {
-    services.qbittorrent = {
-      enable = true;
+    services = {
+      qbittorrent = {
+        enable = true;
+        package = pkgs.qbittorrent-enhanced-nox;
+      };
+      peer-ban-helper.enable = true;
     };
 
-    intransience.datastores.persist.dirs = [ config.services.qbittorrent.profileDir ];
+    intransience.datastores.persist.dirs =
+      let
+        cfg = config.services;
+      in
+      [
+        {
+          inherit (cfg.qbittorrent) user group;
+          path = cfg.qbittorrent.profileDir;
+        }
+        {
+          inherit (cfg.peer-ban-helper) user group;
+          path = cfg.peer-ban-helper.dataDir;
+        }
+      ];
   };
 }
