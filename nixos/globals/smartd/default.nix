666b0935fc45441b07fda64cbd83dc5a9b97cc0c
LiAlH4 <lialh4qwq@outlook.com>
2026-08-11 13:55:35 +0800

    update smartd settings

--- nixos/globals/smartd/default.nix
+++ nixos/globals/smartd/default.nix
@@ -9,14 +9,16 @@
     enable = true;
     defaults.monitored =
       let
-        cat = pkgs.uutils-coreutils-noprefix |> (p: "${p}/bin/cat") |> lib.escapeShellArg;
+        tg-transient-wrapped = pkgs.tg-transient.wrap {
+          tokenFile = config.age.secretsV2.smartd.bot.token;
+          chatFile = config.age.secretsV2.smartd.bot.target;
+        };
+        tg-transient-wrapped-sh-arg = tg-transient-wrapped |> lib.getExe |> lib.escapeShellArg;
         notify = pkgs.writers.writeFishBin "smartd-notify" ''
-          set -x TG_TRANSIENT_TOKEN (${cat} ${lib.escapeShellArg config.age.secretsV2.smartd.bot.token})
-          ${
-            pkgs.tg-transient |> lib.getExe |> lib.escapeShellArg
-          } (${cat} ${lib.escapeShellArg config.age.secretsV2.smartd.bot.target}) "Smartd Notification"\n\n"$SMARTD_FAILTYPE"\n\n"$SMARTD_MESSAGE"
+          ${tg-transient-wrapped-sh-arg} "Smartd Notification" \n "$SMARTD_FAILTYPE" \n "$SMARTD_MESSAGE"
         '';
+        notify-sh-arg = notify |> lib.getExe |> lib.escapeShellArg;
       in
-      "-a -m smartd@lialh4.nix -M test -M exec ${lib.getExe notify |> lib.escapeShellArg}";
+      "-a -m dummy -M test -M exec ${notify-sh-arg}";
   };
 }
