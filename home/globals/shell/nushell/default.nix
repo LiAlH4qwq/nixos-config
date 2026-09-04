4ae28cf33202493622e96a7396336ec60165557a
LiAlH4 <lialh4qwq@outlook.com>
2026-09-04 19:20:08 +0800

    add nufmt

--- home/globals/shell/nushell/default.nix
+++ home/globals/shell/nushell/default.nix
@@ -1,8 +1,10 @@
-_: {
+{ pkgs, ... }: {
   programs.nushell = {
     enable = true;
     settings.show_banner = false;
   };
+
+  home.packages = with pkgs; [ nufmt ];
 
   liuxu.home.internal.intransience.files = [ ".config/nushell/history.txt" ];
 }
